2ca4b97fa317321e3d34bc2cf8dadcad0136504a
manu12perez <manu12perez@gmail.com>
2025-03-24 18:33:11 +0100

    Add project files.

---
+++ ApiCoreCrudDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,77 @@
+﻿using ApiCoreCrudDepartamentos.Models;
+using ApiCoreCrudDepartamentos.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCoreCrudDepartamentos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Departamento>>> GetDepartamentos()
+        {
+            return await this.repo.GetDepartamentosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Departamento>> FindDepartamento(int id)
+        {
+            return await this.repo.FindDepartamentoAsync(id);
+        }
+
+        //LOS METODOS POR DEFECTO DE POST O PUT RECIBEN UN OBJETO
+        //SI QUEREMOS ENVIAR PARAMETROS, DEBEMOS MAPEARLOS CON ROUTING
+        [HttpPost]
+        public async Task<ActionResult> InsertDepartamento(Departamento departamento)
+        {
+            await this.repo.InsertDepartamentoAsync(departamento.IdDepartamento
+                , departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+
+        //EL METODO POR DEFECTO DE DELETE RECIBE UN ID
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteDepartamento(int id)
+        {
+            await this.repo.DeleteDepartamentoAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateDepartamento(Departamento departamento)
+        {
+            await this.repo.UpdateDepartamentoAsync(departamento.IdDepartamento
+                , departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+
+        //PODEMOS PERSONALIZAR METODOS POST, PUT O DELETE A CONVENIENCIA
+        [HttpPost]
+        [Route("[action]/{id}/{nombre}/{localidad}")]
+        public async Task<ActionResult> PostDepartamento(int id, string nombre, string localidad)
+        {
+            await this.repo.InsertDepartamentoAsync(id, nombre, localidad);
+            return Ok();
+        }
+
+        //SI LO NECESITAMOS, PODEMOS COMBINAR OBJETOS CON PARAMETROS.
+        //EL OBJETO ES EL ULTIMO ELEMENTO QUE SE INCLUYE EN LA PETICION
+        //DEL METODO
+        [HttpPut]
+        [Route("[action]/{id}")]
+        public async Task<ActionResult> PutDepartamento(int id, Departamento departamento)
+        {
+            await this.repo.UpdateDepartamentoAsync(id, departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+    }
+}
---
+++ ApiCoreCrudDepartamentos/Program.cs
@@ -0,0 +1,41 @@
+using ApiCoreCrudDepartamentos.Data;
+using ApiCoreCrudDepartamentos.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+/***********************************************************************************************************/
+string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
+builder.Services.AddDbContext<DepartamentosContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddTransient<RepositoryDepartamentos>();
+/***********************************************************************************************************/
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+}
+/***********************************************************************************************************/
+app.MapOpenApi();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "Api Crud Departamentos");
+    options.RoutePrefix = "";
+});
+/***********************************************************************************************************/
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
